cfdb0db98055a8b996de35fab4de928258ee29eb
Fabio Lima <fabionl@gmail.com>
2014-07-14 17:56:58 -0300

    My version of cachematrix.R for the Programming Assignment 2 of the R Programming Course, on Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allows to cache the inverse of a matrix.
+##   To use these function, you should create a squared matrix, applying it to
+##     the makeCacheMatrix function. Then, you should apply the makeCacheMatrix
+##     object to the cacheSolve function.
+##       Example:
+##         source("cachematrix.R")
+##         mcm <- makeCacheMatrix(x)
+##         mcm$getsolve()   # NULL
+##         cacheSolve(mcm)  # does the inversion and returns the inversed matrix.
+##         mcm$getsolve()   # shows the inversed matrix.
+##         cacheSolve(mcm)  # displays a message the its getting the cached data and returns the inversed matrix.
 
-## Write a short comment describing this function
 
+
+## Creates a cacheable matrix from a given matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## Work with the cacheable matrix, allowing to use the cache when the matrix
+##   has not yet been used.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
